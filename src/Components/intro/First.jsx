ba493eba8dc09ccb5af3b3e030b9de5da28eff86
ALowton <ALowton@QA.com>
2021-05-24 11:38:50 +0100

    AL: init commit

---
+++ src/Components/intro/First.jsx
@@ -0,0 +1,8 @@
+
+const First = () => {
+    return (
+        <h3> This is a new element! </h3>
+    )
+}
+
+export default First;
---
+++ src/Components/intro/Second.jsx
@@ -0,0 +1,15 @@
+
+const Second = () => {
+    const NAME = "Anoush";
+    const DATE = new Date();
+
+    return (
+        <>
+            <h1> Here is another element! </h1>
+            <p>{NAME}</p>
+            <p>{DATE.toLocaleDateString}</p>
+        </>
+    )
+}
+
+export default Second;
